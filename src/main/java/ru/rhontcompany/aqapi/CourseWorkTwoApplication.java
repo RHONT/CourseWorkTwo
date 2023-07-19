f11a33b32427cd1573be2f84d2efae005f207df1
RHONT <rg915i@yandex.ru>
2023-07-19 22:36:21 +0300

    Initial commit

---
+++ src/main/java/ru/rhontcompany/aqapi/CourseWorkTwoApplication.java
@@ -0,0 +1,13 @@
+package ru.rhontcompany.aqapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseWorkTwoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseWorkTwoApplication.class, args);
+    }
+
+}
